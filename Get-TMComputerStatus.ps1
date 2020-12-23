7abeeadc542e16b4d50f88aae48a6d14cc581d55
Craig Larkin <craig.larkin@property.nsw.gov.au>
2020-12-23 16:05:25 +1100

    Initial commit of files to source control

---
+++ Get-TMComputerStatus.ps1
@@ -0,0 +1,20 @@
+function Get-TMComputerStatus {
+    [CmdletBinding()]
+    param (
+        [string[]] $ComputerName,
+        [string] $ErrorLogFilePath,
+        [switch] $LogErrors
+    )
+    
+    BEGIN {
+        
+    }
+    
+    PROCESS {
+        
+    }
+    
+    END {
+        
+    }
+}
