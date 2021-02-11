9205277fe93033d4f06a23b7ec57771e13f0b3bc
Craig Larkin <craig.larkin@property.nsw.gov.au>
2021-02-11 17:48:50 +1100

    Added initial calls to get computer information

--- Get-TMComputerStatus.ps1
+++ Get-TMComputerStatus.ps1
@@ -3,6 +3,7 @@ function Get-TMComputerStatus {
     param (
         [string[]] $ComputerName,
         [string] $ErrorLogFilePath,
+        #[string] $Credential,
         [switch] $LogErrors
     )
     
@@ -11,10 +12,30 @@ function Get-TMComputerStatus {
     }
     
     PROCESS {
-        
+        foreach ($Computer in $ComputerName) {
+            ## Run the commands here
+            $os = Get-CimInstance win32_operatingsystem #-computername $computer |
+            | Select-Object -property CSName, TotalVisibleMemorySize, FreePhysicalMemory,
+            NumberOfProcesses, #lastBootUpTime, 
+            @{Name = "PctFreeMemory"; Expression = { ($_.freephysicalmemory / `
+                        ($_.TotalVisibleMemorySize)) * 100 }
+            },
+            @{Name = "Uptime"; Expression = { (Get-Date) - $_.lastBootUpTime } }
+
+            $cpu = Get-CimInstance win32_processor #-ComputerName $computer |
+            | Select-Object -Property LoadPercentage
+
+            $vol = Get-Volume -DriveLetter C |
+            Select-Object -property @{Name = "PctFreeC"; Expression = { ($_.SizeRemaining / $_.size) * 100 }
+            }
+
+            $os, $cpu, $vol
+
+        } # End foreach $computer
+    
     }
     
     END {
         
     }
-}
+} #Get-TMComputerStatus
