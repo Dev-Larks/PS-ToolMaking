e7c71ddefc7ada892b493aa6595a285db269aee1
Craig Larkin <craig.larkin@dpie.nsw.gov.au>
2025-06-18 20:35:16 +1000

    Removal of renamed/redundant files

---
+++ Part 1.3/Get-TMRemoteListeningConfiguration.ps1
@@ -0,0 +1,33 @@
+function Get-TMRemoteListeningConfiguration {
+    [CmdletBinding()]
+    param (
+        [string] $ComputerName,
+        [switch] $LogErrors,
+        [string] $LogFilePath = C:\Temp\OfflineServers.txt
+    )
+    
+    begin {
+        $ReportDate = Get-Date
+    }
+    
+    process {
+
+        $ports = "22", "5985", "5986"
+
+        foreach ($computer in $ComputerName) {
+            
+            foreach ($port in $ports) {
+                <# $port is tports item #>
+            
+            Test-NetConnection -Computername $computer -Port $port | Select-Object Computer,RemotePort, TcpTestSucceeded
+
+            }# End ForEach port
+
+        }# End ForEach computer
+    
+    }
+    
+    end {
+        
+    }
+}
